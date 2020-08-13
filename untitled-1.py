6ea8a871bcc0ed63c6e130aa54b17a95a9f7b896
Wujt <wuqqq520@qq.com>
2020-08-13 13:30:04 +0800

    updata

--- untitled-1.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-import time
-
-print('/Test_report/'+time.strftime("%Y%m%d%H%M%S")+'.html')
-report_file='./Test_report/'+time.strftime("%Y%m%d%H%M%S")+'.html'
-f=open(report_file,"w",encoding='utf-8')
-f.close()
