3d7c83cdfe2fa5bef57a3476619bfb5bb3bed699
wuqqq520 <53115870+wuqqq520@users.noreply.github.com>
2020-08-14 16:39:35 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -3,11 +3,11 @@
 #邮件相关配置************************
 mail_host="smtp.mxhichina.com"     
 #邮箱服务器
-mail_user="wujietao@ellabook.cn"   
+mail_user="361576548@qq.cn"   
 #邮箱账号
-mail_pass="Ella123456"             
+mail_pass="****"             
 #邮箱密码
-receivers = ['wujietao@ellabook.cn','wuqqq520@qq.com']
+receivers = ['wuqqq520@qq.com']
 #收件人
 subject="Funbook test report"  
 #邮件标题
